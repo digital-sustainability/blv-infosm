c126d9395f6101cf6fdad1868c9e63cbaa6d9443
alkraeuchi <alkraeuchi@users.noreply.github.com>
2019-04-23 14:05:14 +0200

    try standard method

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -4,27 +4,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-declare global {
-  interface Window {
-    WebComponents: {
-      ready: boolean;
-    };
-  }
-}
+// declare global {
+//   interface Window {
+//     WebComponents: {
+//       ready: boolean;
+//     };
+//   }
+// }
 
 if (environment.production) {
   enableProdMode();
 }
 
-function bootstrapModule() {
+// function bootstrapModule() {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
-}
+// }
 
-if (window.WebComponents.ready) {
-  // Web Components are ready
-  bootstrapModule();
-} else {
-  // Wait for polyfills to load
-  window.addEventListener('WebComponentsReady', bootstrapModule);
-}
+// if (window.WebComponents.ready) {
+//   // Web Components are ready
+//   bootstrapModule();
+// } else {
+//   // Wait for polyfills to load
+//   window.addEventListener('WebComponentsReady', bootstrapModule);
+// }
