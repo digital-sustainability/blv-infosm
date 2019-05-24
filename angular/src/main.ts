3325959a2e8fc322931b6c31cd72c307d6b245a3
tpluscode <tpluscode@users.noreply.github.com>
2019-05-24 10:24:37 +0200

    transpile maps, add polyfills

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -4,27 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// declare global {
-//   interface Window {
-//     WebComponents: {
-//       ready: boolean;
-//     };
-//   }
-// }
-
 if (environment.production) {
   enableProdMode();
 }
 
-// function bootstrapModule() {
+window.addEventListener('WebComponentsReady', () => {
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
-// }
-
-// if (window.WebComponents.ready) {
-//   // Web Components are ready
-//   bootstrapModule();
-// } else {
-//   // Wait for polyfills to load
-//   window.addEventListener('WebComponentsReady', bootstrapModule);
-// }
+});
