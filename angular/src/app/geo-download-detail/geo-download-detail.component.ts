9ba7fe81a52a94209f3adc6a66168db33fe7dc8a
al-soup <alex@kraeuchi.io>
2019-06-12 18:07:02 +0200

    Finish geo-download
    -Add check for terms acceptance
    -Add navigation
    -Add warning prompt CSS
    -Dev build
    refs #1743 @1h
    
    Note: Link integration still missing

--- angular/src/app/geo-download-detail/geo-download-detail.component.ts
+++ angular/src/app/geo-download-detail/geo-download-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
+import { start } from 'repl';
 
 @Component({
   selector: 'app-geo-download-detail',
@@ -8,19 +10,31 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class GeoDownloadDetailComponent implements OnInit {
 
+  checked = false;
+  hideWarning = true;
+
   constructor(
     public translator: TranslateService,
+    private _router: Router
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  onCheck(event): void {
+    this.checked = event.target.checked;
   }
 
   onDownload() {
-
+    if (this.checked) {
+      console.log('Start download');
+      this._router.navigate(['/info']);
+    } else {
+      this.hideWarning = false;
+    }
   }
 
   onCancel() {
-
+    this._router.navigate(['/info']);
   }
 
 }
