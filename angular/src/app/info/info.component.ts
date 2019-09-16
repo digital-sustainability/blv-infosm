a9d722783f9090254ba9492db2d9f9016fda1f65
jonas <jonas@schwery.com>
2019-09-16 08:56:23 +0200

    add info pictures in the languages 'en', 'fr' and 'it'
    refs #2103 @0.25

--- angular/src/app/info/info.component.ts
+++ angular/src/app/info/info.component.ts
@@ -1,19 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../shared/services/language.service';
+import { Subscription } from 'rxjs';
+import { NotificationService } from '../shared/services/notification.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.css']
 })
-export class InfoComponent {
+export class InfoComponent implements OnInit, OnDestroy {
+
+  private _langSub: Subscription;
+  public currentLang: string;
 
   constructor(
     public translator: TranslateService,
+    private _langauageService: LanguageService,
+    private _notification: NotificationService
   ) { }
+
+  ngOnInit() {
+    this._langSub = this._langauageService.currentLang.subscribe(
+      lang => {
+       this.currentLang = lang;
+      }, err => {
+        this._notification.errorMessage(err.statusText + '<br>' + 'language error', err.name);
+      }
+    );
+
+  }
 
   onScrollUp(): void {
     window.scrollTo(0, 0);
+  }
+
+  ngOnDestroy() {
+    this._langSub.unsubscribe();
   }
 
 }
